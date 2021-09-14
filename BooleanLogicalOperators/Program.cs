32e5365c70626a1057715b39d319a5b593fcd1c5
Opilane TTHK <opilane@tthk.ee>
2021-09-14 12:08:51 +0300

    comment

---
+++ BooleanLogicalOperators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BooleanLogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt sisestada kasutajatunnus;
+            //programm küsib kasutajal sisestada parool;
+            //programm kontrollib, kas mõlemad, nii kasutajatunnus
+            //kui ka parool on õiged;
+            //kui mõlemad, kasutajatunnus ja parool on õiged
+            //programm kuvab konsoolis "Tere tulemast!"
+            //kui kasutajatunnus või parool on vale, siis 
+            //programm kuvab "sisselogimine ebaõnnestus, proovi uuesti."
+            //kt: admin, pr:admim1234
+
+            Console.WriteLine("Sisesta kasutajatunnus");
+            String userName = Console.ReadLine();
+            Console.WriteLine("Sisesta parool:");
+            string userPassword = Console.ReadLine();
+
+            if (userName == "admin" && userPassword == "admin1234")
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+            else
+            {
+                Console.WriteLine("Sisselogimine ebaõnnestus.Proovi uuesti.");
+
+            }
+            else
+            {
+                Console.WriteLine("Tere tulemast!");
+
+        }
+    }
+}
